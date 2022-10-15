4a55b79ee5c7325916afc8885c3edc5e18ce3681
taj <ftem.taj@gmail.com>
2022-10-15 16:56:58 +0330

    best practice

--- leap.js
+++ leap.js
@@ -4,7 +4,5 @@
 //
 
 export const isLeap = (year) => {
-  if (year % 400 == 0) return true;
-  if (year % 100 != 0 && year % 4 == 0) return true;
-  else return false;
+  return year % 400 === 0 || (year % 100 !== 0 && year % 4 === 0);
 };
